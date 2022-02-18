de7f12fc59b1a11ba01c792869d76c40f5744a49
AnthonRM <AntonioRM270601@gmail.com>
2022-02-17 23:58:44 -0600

    Se agrego el apartado de PaymentFormat

---
+++ app/src/main/java/org/equiposeis/huellitasaventureras/ui/PaymentFormat.java
@@ -0,0 +1,25 @@
+package org.equiposeis.huellitasaventureras.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.equiposeis.huellitasaventureras.R;
+import org.equiposeis.huellitasaventureras.databinding.FragmentAddPetBinding;
+import org.equiposeis.huellitasaventureras.databinding.FragmentPaymentFormatBinding;
+
+
+public class PaymentFormat extends Fragment {
+
+    //Variables de PaymentFormat
+    private FragmentPaymentFormatBinding binding;
+    private String TitularCard,Introducir;
+    private int CVV ;
+
+
+
+}
