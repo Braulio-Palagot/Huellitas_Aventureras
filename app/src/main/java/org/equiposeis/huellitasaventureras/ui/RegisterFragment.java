debf41f8f07357b8860601f504d7b25f32d282da
Ceffyl_GitKraken <bdpalagot@gmail.com>
2022-02-18 14:37:35 -0600

    Actualización de RegisterFragment
    Cambios Realizados:
        1) Se actualizó el layout del fragment para utilizar los elementos de Material Design con los estilos establecidos anteriormente.
        2) Se actualizó el código para adaptarse a los nuevos elementos de Material Design; especialmente los DropDown.
        3) Se creó el layout para elementos de los DropDown.
        4) Se crearon las strings necesarias para el RegisterFragment en el archivo strings de Android.

--- app/src/main/java/org/equiposeis/huellitasaventureras/ui/RegisterFragment.java
+++ app/src/main/java/org/equiposeis/huellitasaventureras/ui/RegisterFragment.java
@@ -7,16 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import org.equiposeis.huellitasaventureras.R;
 import org.equiposeis.huellitasaventureras.databinding.FragmentProfileBinding;
 import org.equiposeis.huellitasaventureras.databinding.FragmentRegisterBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class RegisterFragment extends Fragment {
 
     private FragmentRegisterBinding binding;
+    // Creación de variables para mandar a la BD:
     private String name, addres, mail, passOne, passTwo;
     private int gender, age, phone, type;
+    // Se crean los arrays de elemntos de los DropDown:
+    private final String[] genders = getResources().getStringArray(R.array.genders);
+    private final String[] user_types = getResources().getStringArray(R.array.user_types);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,32 +34,50 @@ public class RegisterFragment extends Fragment {
         binding = FragmentRegisterBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        if (type == 0) { //type se baja de la base de datos.
-            binding.txtName.setVisibility(View.GONE);
-            binding.txtAddres.setVisibility(View.GONE);
-            binding.txtMail.setVisibility(View.GONE);
-        }
+        // Se llenan los DropDown con los arrays creados:
+        binding.txtGender.setAdapter(new ArrayAdapter(requireActivity(), R.layout.dropdown_item, genders));
+        binding.txtUserType.setAdapter(new ArrayAdapter(requireActivity(), R.layout.dropdown_item, user_types));
 
+        // Se crea el click listener para registrarse:
         binding.bttnRegister.setOnClickListener(v -> {
-            name = binding.txtName.getText().toString();
-            addres = binding.txtAddres.getText().toString();
-            mail = binding.txtMail.getText().toString();
-            passOne = binding.txtPasswordOne.getText().toString();
-            passTwo = binding.txtPasswordTwo.getText().toString();
+            // Se toman los valores string de los campos de texto, confirmando que no sean nulos:
+            name = Objects.requireNonNull(binding.txtName.getText()).toString();
+            addres = Objects.requireNonNull(binding.txtAddress.getText()).toString();
+            mail = Objects.requireNonNull(binding.txtMail.getText()).toString();
+            passOne = Objects.requireNonNull(binding.txtPassword.getText()).toString();
+            passTwo = Objects.requireNonNull(binding.txtRepeatPassword.getText()).toString();
 
-            age = Integer.parseInt(binding.txtAge.getText().toString());
-            phone = Integer.parseInt(binding.txtPhone.getText().toString());
-            gender = binding.spnrGender.getSelectedItemPosition();
-            type = binding.spnrType.getSelectedItemPosition();
+            // Se toman los valores numéricos de de edad y teléfono:
+            age = Integer.parseInt(Objects.requireNonNull(binding.txtAge.getText()).toString());
+            phone = Integer.parseInt(Objects.requireNonNull(binding.txtPhone.getText()).toString());
 
+            // Se selecciona el valor índice de cada selección de los DropDowns:
+            if (binding.txtGender.getText().toString().equals("Masculino")) {
+                gender = 0;
+            } else if (binding.txtGender.getText().toString().equals("Femenino")) {
+                gender = 1;
+            } else {
+                gender = 2;
+            }
+            if (binding.txtUserType.getText().toString().equals("Cliente")) {
+                gender = 0;
+            } else if (binding.txtGender.getText().toString().equals("Paseador")) {
+                gender = 1;
+            }
+
+            // Se confirma que el correo no esté vacío y que las contraseñas coincidan para
+            // realizar el registro:
             if (!mail.isEmpty()) {
-                if (passOne == passTwo) {
+                if (passOne.equals(passTwo)) {
                     //Mandar datos a la base de datos.
+                    requireActivity().onBackPressed();
+                } else {
+                    Toast.makeText(requireActivity(), R.string.not_loged, Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        binding.bttnCancel.setOnClickListener(v -> getActivity().onBackPressed());
+        binding.bttnCancel.setOnClickListener(v -> requireActivity().onBackPressed());
 
         return root;
     }
