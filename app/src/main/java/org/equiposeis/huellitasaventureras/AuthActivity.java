593c867be4bf37897f240ac702f8bdfa068aac20
Ceffyl_GitKraken <bdpalagot@gmail.com>
2022-02-19 18:48:50 -0600

    Permisos de almacenamiento e internet
    Cambios Realizados:
        1) Se agregaron los permisos de Internet y de Almacenamiento externo al AndroidManifest.xml.
        2) Se crearon los métodos para revisar y solicitar, en caso de ser necesario, los permisos agregados al AndroidManifest.xml.

--- app/src/main/java/org/equiposeis/huellitasaventureras/AuthActivity.java
+++ app/src/main/java/org/equiposeis/huellitasaventureras/AuthActivity.java
@@ -1,10 +1,13 @@
 package org.equiposeis.huellitasaventureras;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,6 +21,7 @@ public class AuthActivity extends AppCompatActivity {
     public static SharedPreferences preferences;
     public static final String PREFS_NAME = "org.equiposeis.huellitasaventureras.sharedpreferences";
     public static final String IS_LOGGED = "IS_LOGGED";
+    public static final int PERMISSION_ID = 34;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,10 @@ public class AuthActivity extends AppCompatActivity {
             this.finish();
         }
 
+        if (!checkPermission()) {
+            requestPermissions();
+        }
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -41,4 +49,11 @@ public class AuthActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     }
 
+    private boolean checkPermission() {
+        return ((ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) && (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED));
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET, Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_ID);
+    }
 }
