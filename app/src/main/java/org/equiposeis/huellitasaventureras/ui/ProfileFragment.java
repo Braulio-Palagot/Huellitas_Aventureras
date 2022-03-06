8ff9f2027e6b9fb4fc99871e7794d2940cf855b8
Ceffyl_GitKraken <bdpalagot@gmail.com>
2022-03-05 22:24:25 -0600

    Funcionalidad de ProfileFragment
    Cambios Realizados:
        1) Se agregó la llamada de datos del usuario desde la base de datos.

--- app/src/main/java/org/equiposeis/huellitasaventureras/ui/ProfileFragment.java
+++ app/src/main/java/org/equiposeis/huellitasaventureras/ui/ProfileFragment.java
@@ -1,53 +1,81 @@
 package org.equiposeis.huellitasaventureras.ui;
 
+import static org.equiposeis.huellitasaventureras.AuthActivity.auth;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.equiposeis.huellitasaventureras.R;
 import org.equiposeis.huellitasaventureras.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
 
-private FragmentProfileBinding binding;
-private int user =0;
+    private FragmentProfileBinding binding;
+    private FirebaseUser user = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-    binding = FragmentProfileBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-    //Jalar datos de la BD
-        //Ocultar datos de paseador
-if (user==0){
-    binding.txtViewPet.setVisibility(View.VISIBLE);
-    binding.rclrPet.setVisibility(View.VISIBLE);
-    binding.bttnAddPet.setVisibility(View.VISIBLE);
-    binding.txtViewWalker.setVisibility(View.GONE);
-    binding.rclrRides.setVisibility(View.GONE);
-} else { //ocultar datos de cliente
-    binding.txtViewPet.setVisibility(View.GONE);
-    binding.rclrPet.setVisibility(View.GONE);
-    binding.bttnAddPet.setVisibility(View.GONE);
-    binding.txtViewWalker.setVisibility(View.VISIBLE);
-    binding.rclrRides.setVisibility(View.VISIBLE);
-}
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //Jalar datos de la BD
+        user = auth.getCurrentUser();
+        db.collection("Usuarios").document(user.getUid()).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    binding.txtName.setText(document.get("Nombre").toString());
+                    binding.txtAddress.setText(document.get("Domicilio").toString());
 
-    binding.bttnAddPet.setOnClickListener( v -> {
-        //Navegación hacia el fragment agregar mascota
-        NavHostFragment.findNavController(this).navigate(R.id.action_navigation_profile_to_navigation_add_pet, null);
+                    //Ocultar datos de paseador
+                    if (Integer.parseInt(document.get("Tipo_Usuario").toString()) == 0) {
+                        binding.txtViewPet.setVisibility(View.VISIBLE);
+                        binding.rclrPet.setVisibility(View.VISIBLE);
+                        binding.bttnAddPet.setVisibility(View.VISIBLE);
+                        binding.txtViewWalker.setVisibility(View.GONE);
+                        binding.rclrRides.setVisibility(View.GONE);
+                    } else if (Integer.parseInt(document.get("Tipo_Usuario").toString()) == 1) { //ocultar datos de cliente
+                        binding.txtViewPet.setVisibility(View.GONE);
+                        binding.rclrPet.setVisibility(View.GONE);
+                        binding.bttnAddPet.setVisibility(View.GONE);
+                        binding.txtViewWalker.setVisibility(View.VISIBLE);
+                        binding.rclrRides.setVisibility(View.VISIBLE);
+                    }
+                    // Se cargan las mascotas al RecyclerView.
+                } else {
+                    Toast.makeText(requireActivity(), "No es posible cargar los datos del usuario en este momento.", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Log.e("USER_NOT_FOUND", "El usuario no se halló: ", task.getException());
+            }
+        });
+
+        binding.bttnAddPet.setOnClickListener(v -> {
+            //Navegación hacia el fragment agregar mascota
+            NavHostFragment.findNavController(this).navigate(R.id.action_navigation_profile_to_navigation_add_pet, null);
+        });
+
+        return root;
     }
-);
 
-    return root;
-    }
-
-@Override
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
