f55aff07aa7fbe861fab5a126ae2a564bbef1b42
Ceffyl_GitKraken <bdpalagot@gmail.com>
2022-03-17 15:25:04 -0600

    UPDATE RideRequestClientFragment
    Cambios Realizados:
        1) Se completó la lógica para subir solicitudes de paseo a la base de datos.
        2) Se cambió el diseño de la clase Paseo para poder simplificar la lógica de la subida de solicitudes.
    
    Cambios Pendientes:
        1) Implementar una lógica similar a la del AddPetFragment para el correcto funcionamiento de los RecyclerViews de Home.

--- app/src/main/java/org/equiposeis/huellitasaventureras/dataModels/Paseo.kt
+++ app/src/main/java/org/equiposeis/huellitasaventureras/dataModels/Paseo.kt
@@ -1,7 +1,9 @@
 package org.equiposeis.huellitasaventureras.dataModels
 
 data class Paseo(
-        var id_paseo:String="",
+        var id_usuario:String="",
+        var id_paseador: String = "",
+        var mascota: String,
         var duracionPaseo:String= "",
         var Time: String="",
 )
--- app/src/main/java/org/equiposeis/huellitasaventureras/ui/RideRequestClientFragment.java
+++ app/src/main/java/org/equiposeis/huellitasaventureras/ui/RideRequestClientFragment.java
@@ -1,28 +1,24 @@
 package org.equiposeis.huellitasaventureras.ui;
 
-import static org.equiposeis.huellitasaventureras.AuthActivity.auth;
 import static org.equiposeis.huellitasaventureras.MainActivity.db;
 import static org.equiposeis.huellitasaventureras.MainActivity.employeesQuery;
-import static org.equiposeis.huellitasaventureras.MainActivity.mascotaSeleccionada;
 import static org.equiposeis.huellitasaventureras.MainActivity.mascotasQuery;
+import static org.equiposeis.huellitasaventureras.MainActivity.user;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
-
-
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -37,55 +33,49 @@ import java.util.Map;
 public class RideRequestClientFragment extends Fragment {
 
     private FragmentRideRequestClientBinding binding;
-    private int RideQuestClientEmployee,RideQuestClientPets,RideQuestClientTime;
+    private int RideQuestClientEmployee, RideQuestClientPets, RideQuestClientTime;
 
-    private ArrayList<String> employees = null;
-    private ArrayList<String> Pets = null;
+    private ArrayList<String> employees = new ArrayList<>();
+    private ArrayList<String> Pets = new ArrayList<>();
     private String[] Times = null;
-    private FirebaseUser ride=null;
     private String Times1 = "";
-    private FirebaseUser user= null;
+    private String mascota = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentRideRequestClientBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        user=auth.getCurrentUser();
         Times = getResources().getStringArray(R.array.Times);
-        ride=auth.getCurrentUser();
 
-        Paseo paseo =new Paseo("","","");
-        String id_paseo=paseo.getId_paseo();
-        String duracionPaseo=paseo.getDuracionPaseo();
-        String times1=paseo.getTime();
+        Paseo paseo = new Paseo("", "", "", "", "");
 
         binding.txtEmployee.setAdapter(new ArrayAdapter(requireActivity(), R.layout.dropdown_item, employees));
         binding.txtPet.setAdapter(new ArrayAdapter(requireActivity(), R.layout.dropdown_item, Pets));
         binding.txtTime.setAdapter(new ArrayAdapter(requireActivity(), R.layout.dropdown_item, Times));
 
 //Rellenado de datos Empleados Disponibles subidos en la  BD
-        employeesQuery.addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        mascotasQuery.addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                for (QueryDocumentSnapshot document : employeesQuery.getResult()){
-                    String nombre= document.get("Nombre").toString();
+                for (QueryDocumentSnapshot document : mascotasQuery.getResult()) {
+                    String nombre = document.get("Nombre").toString();
                     Pets.add(nombre);
                 }
-                binding.txtPet.setAdapter(new ArrayAdapter(requireActivity(),R.layout.dropdown_item,Pets));
+                binding.txtPet.setAdapter(new ArrayAdapter(requireActivity(), R.layout.dropdown_item, Pets));
             }
         });
 
 
         //Rellenado de datos de Mascotas registradas subidos en la BD
-        mascotasQuery.addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        employeesQuery.addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                for (QueryDocumentSnapshot document : mascotasQuery.getResult()){
-                    String nombre= document.get("Nombre").toString();
+                for (QueryDocumentSnapshot document : employeesQuery.getResult()) {
+                    String nombre = document.get("Nombre").toString();
                     employees.add(nombre);
                 }
-                binding.txtEmployee.setAdapter(new ArrayAdapter(requireActivity(),R.layout.dropdown_item,employees));
+                binding.txtEmployee.setAdapter(new ArrayAdapter(requireActivity(), R.layout.dropdown_item, employees));
             }
         });
 
@@ -119,36 +109,44 @@ public class RideRequestClientFragment extends Fragment {
                 Times1="135 Mint";
             } else if (binding.txtTime.getText().toString().equals("150 Mint")) {
                 RideQuestClientTime = 8;
-                Times1="150 Mint";
+                Times1 = "150 Mint";
             } else if (binding.txtTime.getText().toString().equals("165 Mint")) {
                 RideQuestClientTime = 9;
-                Times1="165 Mint";
+                Times1 = "165 Mint";
             } else {
                 RideQuestClientTime = 10;
-                Times1="180 Mint";
+                Times1 = "180 Mint";
             }
 
+            if (!binding.txtPet.getText().toString().isEmpty()) {
+                mascota = binding.txtPet.getText().toString();
+            }
 
-
-            paseo.setId_paseo(ride.getUid());
-            paseo.setTime(times1);
+            for (DocumentSnapshot document : employeesQuery.getResult()) {
+                if (document.get("Nombre").toString().equals(binding.txtEmployee.getText().toString())) {
+                    paseo.setId_paseador(document.get("ID_Usuario").toString());
+                    break;
+                }
+            }
+            paseo.setId_usuario(user.getUid());
+            paseo.setMascota(mascota);
+            paseo.setTime(Times1);
 
             //Metodo para mandar datos a la base de datos.
+            Map<String, Object> Paseo_db = new HashMap<>();
+            Paseo_db.put("ID_Usuario", paseo.getId_usuario());
+            Paseo_db.put("Mascota", paseo.getMascota());
+            Paseo_db.put("ID_Paseador", paseo.getId_paseador());
+            Paseo_db.put("Duracion_Paseo", paseo.getTime());
 
-            Map<String, Object> Paseo_db=new HashMap<>();
-            Paseo_db.put("id_Paseo", paseo.getId_paseo());
-            //Paseo_db.put("Mascota", );
-            Paseo_db.put("ID_Cliente", user.getUid());
-            Paseo_db.put("Duracion Paseo", paseo.getTime());
-
-
-            db.collection("Solicitud De Paseo").document(user.getUid())
+            db.collection("Solicitudes de Paseos").document(user.getUid() + mascota)
                     .set(Paseo_db)
                     .addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
-                        public void onSuccess(Void aVoid) {
-                            Toast.makeText(requireActivity(),"Registro de paseo exitoso", Toast.LENGTH_SHORT).show();
-                            NavHostFragment.findNavController(requireParentFragment()).navigate(R.id.action_navigation_add_pet_to_navigation_profile, null);
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(requireActivity(), "Registro de paseo exitoso", Toast.LENGTH_SHORT).show();
+
+                            NavHostFragment.findNavController(requireParentFragment()).navigate(R.id.action_navigation_ride_request_to_navigation_home, null);
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
@@ -157,8 +155,6 @@ public class RideRequestClientFragment extends Fragment {
                             Toast.makeText(requireActivity(),"No se pudo realizar el registro del paseo", Toast.LENGTH_SHORT).show();
                         }
                     });
-            NavHostFragment.findNavController(this).navigate(R.id.action_navigation_ride_request_to_navigation_home, null);
-
         });
 
         binding.bttnCancelClientRequest.setOnClickListener(v -> {
